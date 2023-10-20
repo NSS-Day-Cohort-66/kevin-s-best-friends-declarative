2f20bfe96c7dc70b243c4f1a6f8879f65ca137c7
Helen Esterman <helenesterman99@gmail.com>
2023-10-20 15:02:52 -0400

    added conditionals within the hauler view to ensure that when the expand query existed the hauler objects expanded

--- shipping.sql
+++ shipping.sql
@@ -56,3 +56,14 @@ INSERT INTO `Ship` VALUES (null, "Dickens", 6);
 INSERT INTO `Ship` VALUES (null, "Kunde", 6);
 INSERT INTO `Ship` VALUES (null, "Hermiston", 5);
 -- End block
+
+SELECT 
+h.id, 
+h.name, 
+h.dock_id,
+d.id,
+d.location,
+d.capacity
+FROM Hauler h
+JOIN Dock d
+ON d.id = h.dock_id;
--- views/hauler_view.py
+++ views/hauler_view.py
@@ -4,17 +4,75 @@ from repository import db_get_single, db_get_all, db_delete, db_update, db_creat
 
 
 class HaulerView:
-    def get(self, handler, pk):
+    def get(self, handler, pk, query_params):
         if pk != 0:
-            sql = "SELECT h.id, h.name, h.dock_id FROM Hauler h WHERE h.id = ?"
-            query_results = db_get_single(sql, pk)
-            serialized_hauler = json.dumps(dict(query_results))
+            if "_expand" in query_params:
+                sql = """
+                        SELECT
+                        h.id, 
+                        h.name, 
+                        h.dock_id,
+                        d.id,
+                        d.location,
+                        d.capacity
+                        FROM Hauler h
+                        JOIN Dock d
+                        ON d.id = h.dock_id
+                        WHERE h.id = ?"""
+                query_results = db_get_single(sql, pk)
+                query_results_dict = dict(query_results)
+                dock = {
+                    "id": query_results_dict["dock_id"],
+                    "location": query_results_dict["location"],
+                    "capacity": query_results_dict["capacity"],
+                }
+                hauler = {
+                    "id": query_results_dict["id"],
+                    "name": query_results_dict["name"],
+                    "dock_id": query_results_dict["dock_id"],
+                    "dock": dock,
+                }
+                dictionary_version_of_object = dict(hauler)
+                serialized_hauler = json.dumps(dictionary_version_of_object)
+            else:
+                sql = "SELECT h.id, h.name, h.dock_id FROM Hauler h WHERE h.id = ?"
+                query_results = db_get_single(sql, pk)
+                serialized_hauler = json.dumps(dict(query_results))
 
             return handler.response(serialized_hauler, status.HTTP_200_SUCCESS.value)
         else:
-            sql = "SELECT h.id, h.name, h.dock_id FROM Hauler h"
-            query_results = db_get_all(sql)
-            haulers = [dict(row) for row in query_results]
+            if "_expand" in query_params:
+                sql = """
+                        SELECT
+                        h.id, 
+                        h.name, 
+                        h.dock_id,
+                        d.id dockId,
+                        d.location,
+                        d.capacity
+                        FROM Hauler h
+                        JOIN Dock d
+                        ON d.id = h.dock_id"""
+                query_results = db_get_all(sql)
+                haulers = []
+                for row in query_results:
+                    dock = {
+                        "id": row["dockId"],
+                        "location": row["location"],
+                        "capacity": row["capacity"],
+                    }
+                    hauler = {
+                        "id": row["id"],
+                        "name": row["name"],
+                        "dock_id": row["dock_id"],
+                        "dock": dock,
+                    }
+                    haulers.append(hauler)
+
+            else:
+                sql = "SELECT h.id, h.name, h.dock_id FROM Hauler h"
+                query_results = db_get_all(sql)
+                haulers = [dict(row) for row in query_results]
             serialized_haulers = json.dumps(haulers)
 
             return handler.response(serialized_haulers, status.HTTP_200_SUCCESS.value)
